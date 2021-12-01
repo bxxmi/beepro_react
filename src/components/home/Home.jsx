c56a85c4e157a2f277cfbb3b0979c1c581924a89
보미 <bomi@bomiui-MacBookAir.local>
2021-12-01 22:37:34 +0900

    comment: delete comment

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { useNavigate } from 'react-router';
-import { useEffect } from 'react/cjs/react.development';
+import React from "react";
+import { useNavigate } from "react-router";
+import { useEffect } from "react/cjs/react.development";
 
 const Home = ({ authService }) => {
   const navigate = useNavigate();
 
   const onLogout = () => {
     authService.logout();
-  }
+  };
 
   useEffect(() => {
-    authService.onAuthChange(user => {
+    authService.onAuthChange((user) => {
       if (!user) {
-        navigate('/login');
+        navigate("/login");
       }
-    })
-  })
+    });
+  });
 
   return (
     <div>
